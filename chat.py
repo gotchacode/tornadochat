5d466350c62a4c377550ac79e89668bd6c75e2b8
vinit Kumar <vinit.kumar@changer.nl>
2015-09-09 22:53:17 +0530

    add files for the first time

---
+++ chat.py
@@ -0,0 +1,126 @@
+# Copyright (C)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
+import logging
+import tornado.escape
+import tornado.ioloop
+import tornado.web
+import os.path
+import uuid
+
+
+from tornado.concurrent import Future
+from tornado import gen
+from tornado.options import define, options, parse_command_line
+
+define("port", default=8888, help="run on the given port", type=int)
+define("debug", default=False, help="run in debug mode")
+
+
+class MessageBuffer(object):
+    def __init__(self):
+        self.waiters = set()
+        self.cache = []
+        self.cache_size = 200
+
+    def wait_for_messages(self, cursor=None):
+        result_future = Future()
+        if cursor:
+            new_count = 0
+            for msg in reversed(self.cache):
+                if msg["id"] == cursor:
+                    break
+                new_count += 1
+            if new_count:
+                result_future.set_result(self.cache[-new_count:])
+                return result_future
+        self.waiters.add(result_future)
+        return result_future
+
+    def cancel_wait(self, future):
+        self.waiters.remove(future)
+        future.set_result([])
+
+    def new_messages(self, messages):
+        logging.info("Sending new message to %r listeners", len(self.waiters))
+        for future in self.waiters:
+            future.set_result(messages)
+        self.waiters = set()
+        self.cache.extend(messages)
+        if (len(self.cache) > self.cache_size):
+                self.cache = self.cache[-self.cache_size:]
+
+global_message_buffer = MessageBuffer()
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html", messages=global_message_buffer.cache)
+
+
+class MessageNewHandler(tornado.web.RequestHandler):
+    def post(self):
+        message = {
+            "id": str(uuid.uuid4()),
+            "body": self.get_argument("body"),
+        }
+        print message
+        message["html"] = tornado.escape.to_basestring(
+            self.render_string("message.html", message=message))
+
+        if self.get_argument("next", None):
+            self.redirect(self.get_argument("next"))
+        else:
+            self.write(message)
+        global_message_buffer.new_messages([message])
+
+
+class MessageUpdatesHandler(tornado.web.RequestHandler):
+    @gen.coroutine
+    def post(self):
+        cursor = self.get_argument("cursor", None)
+        # Save the future returned by wait_for_messages so we can cancel
+        # it in wait_for_messages
+        self.future = global_message_buffer.wait_for_messages(cursor=cursor)
+        messages = yield self.future
+        if self.request.connection.stream.closed():
+            return
+        self.write(dict(messages=messages))
+
+    def on_connection_close(self):
+        global_message_buffer.cancel_wait(self.future)
+
+
+def main():
+    parse_command_line()
+    app = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+            (r"/a/message/new", MessageNewHandler),
+            (r"/a/message/updates", MessageUpdatesHandler),
+        ],
+        cookie_secret="adnadfhaihjngjwgegegjsofsknfmadajdn",
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        xsrf_cookies=False,
+        debug=options.debug,
+    )
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+if __name__ == "__main__":
+    main()
